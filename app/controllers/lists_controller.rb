5e1db62e7f8977b8dd99bd91961f19a2f5e1b103
millene prado <millene_vprado@hotmail.com>
2021-08-31 11:47:37 -0300

    add new & create actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,20 @@
 class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    # if @list.save
+    #   redirect_to list_path(@list)
+    # else
+    #   render :new
+    # end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
