9ec5177b30c3372ea243b821fb407c2455cf6d94
millene prado <millene_vprado@hotmail.com>
2021-08-31 13:51:34 -0300

    update list model validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :products, dependent: :destroy
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: { unless: -> { title == 'default' } }
 end
