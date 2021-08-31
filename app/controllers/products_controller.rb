a6d7d211a61d66cbbbe313d27121d7942606dc04
Raphael Cruz <raphacruz_90@hotmail.com>
2021-08-31 16:32:20 -0300

    pundit product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+  def index
+    @products = policy_scope(Product)
+  end
+
+  def show
+    authorize @product
+  end
+
+  def new
+    @product = Product.new
+    authorize @product
+  end
+
+  def create
+    authorize @product
+  end
+
+  def destroy
+    authorize @product
+  end
 end
