cef917008b694753f807a41fcce0205dc3f8879a
Raphael Cruz <raphacruz_90@hotmail.com>
2021-08-31 16:22:53 -0300

    admin implemented

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,30 @@
 class ListsController < ApplicationController
+  def index
+    @lists = policy_scope(List)
+  end
+
+  def show
+    authorize @list
+  end
+
+  def new
+    @list = List.new
+    authorize @list
+  end
+
+  def create
+    authorize @list
+  end
+
+  def edit
+    authorize @list
+  end
+
+  def update
+    authorize @list
+  end
+
+  def destroy
+    authorize @list
+  end
 end
