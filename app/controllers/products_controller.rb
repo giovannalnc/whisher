ebacbabe1ca8c340c8d7ad5b6e74d05222fc7e85
Giovanna Leite Nunes Coelho <giovanna.lnc02@gmail.com>
2021-08-31 20:55:13 -0300

    index and new done, and create in progress

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,33 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    url = @product.url
+
+    html_file = HTTParty.get(url)
+    html_doc = Nokogiri::HTML(html_file)
+
+    @product.name = html_doc.search('.product-title').text.strip
+    price = html_doc.search('h2').text.strip
+    @product.price = price[2..].gsub(',', '.').to_f
+    @product.inventory = html_doc.search('h4 b').text.strip
+    if @product.save
+      redirect_to product_path(@product), notice: 'Product was successfully added.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:url)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_181507) do
+ActiveRecord::Schema.define(version: 2021_08_31_160759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_08_30_181507) do
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
     t.index ["list_id"], name: "index_products_on_list_id"
   end
 
