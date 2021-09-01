0317d1a2b4243fa50fb11bbcf4ac4fdb135381cb
Giovanna Leite Nunes Coelho <giovanna.lnc02@gmail.com>
2021-09-01 19:11:22 -0300

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_141843) do
+ActiveRecord::Schema.define(version: 2021_09_01_185907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_09_01_141843) do
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
+    t.string "photo"
     t.index ["list_id"], name: "index_products_on_list_id"
   end
 
@@ -41,7 +43,7 @@ ActiveRecord::Schema.define(version: 2021_09_01_141843) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
