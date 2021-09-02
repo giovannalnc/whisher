d56a6e6dea8482a523af50282c47f4d7da3d0b15
Igor Areco <igorhareco@gmail.com>
2021-09-02 15:22:10 -0300

    added default 'Available' to inventory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_185907) do
+ActiveRecord::Schema.define(version: 2021_09_02_181838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_09_01_185907) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.float "price"
-    t.string "inventory"
+    t.string "inventory", default: "Available"
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2021_09_01_185907) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin", default: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
